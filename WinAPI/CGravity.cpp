6f7e88d266507e424b431341f74de2a7a219ba37
limBirdSpring <doslrh15@naver.com>
2022-11-03 16:27:33 +0900

    feat : 중력 추가

---
+++ WinAPI/CGravity.cpp
@@ -0,0 +1,45 @@
+#include "framework.h"
+#include "CGravity.h"
+#include "CTimeManager.h"
+#include "CGameObject.h"
+
+CGravity::CGravity()
+{
+	m_fVelocity = 1;
+	m_ground = 0;
+	m_platform = 0;
+}
+
+CGravity::~CGravity()
+{
+}
+
+void CGravity::Init()
+{
+}
+
+void CGravity::Update()
+{
+
+}
+
+void CGravity::PhysicsUpdate()
+{
+	if (m_ground == 0 && m_platform == 0)
+	{
+		GetOwner()->SetPos(GetOwner()->GetPos().x, GetOwner()->GetPos().y + DT * m_fVelocity);
+		m_fVelocity += 450 * DT;
+
+		if (m_fVelocity > 1000)
+			m_fVelocity = 1000;
+	}
+
+}
+
+void CGravity::Render()
+{
+}
+
+void CGravity::Release()
+{
+}
---
+++ WinAPI/CGravity.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "CComponent.h"
+
+class CGameObject;
+
+class CGravity : public CComponent
+{
+	friend CGameObject;
+
+public:
+	CGravity();
+	virtual ~CGravity();
+
+private:
+	const float gravity = 25.f;
+	float m_fVelocity;//속도
+	int m_ground; //땅인지 여부
+	int m_platform; //플랫폼인지 여부
+
+	void Init() override;
+	void Update() override;
+	void PhysicsUpdate() override;
+	void Render() override;
+	void Release() override;
+
+protected:
+
+	void SetVelocity(float velocity) { m_fVelocity = velocity; }
+};
+
+//y = DT x velocity
+
+//velocity - = 25xDT
